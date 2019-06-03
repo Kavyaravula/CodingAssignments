d3a8e28170574ff08a7be814ea3f0b06d12aebd7
Kavyaravula <seleniumlearning18@gmail.com>
2019-06-03 10:52:29 -0700

    Added utility class

---
+++ src/main/java/com/qa/zoopla/Utility/Utility.java
@@ -0,0 +1,12 @@
+package com.qa.zoopla.Utility;
+
+import com.qa.zoopla.TestBase.TestBase;
+
+public class Utility extends TestBase{
+	
+	
+	public static void readData(){
+		System.out.println("To read data from Excel");
+	}
+
+}
